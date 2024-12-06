b76108aaf8af747d68127a3aedf458c5cc3935f4
nattapong_2005 <azzzx30089@gmail.com>
2024-12-06 18:58:27 +0700

    first commit

---
+++ src/Controllers/product.controller.js
@@ -0,0 +1,70 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+exports.get = async (req, res) => {
+//   SELECT * FROM Product;
+  const products = await prisma.product.findMany({
+    include: { 
+        Category: true,
+     }
+  });
+  res.json(products);
+};
+
+exports.getById = async (req, res) => {
+  const { id } = req.params;
+//   SELECT * FROM Product WHERE id = [id]
+  const product = await prisma.product.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+    include: {
+        Category: true,
+    }
+  });
+  res.json(product);
+};
+
+exports.create = async (req, res) => {
+  const { category_id, name, price, description, unit_in_stock } = req.body;
+  const product = await prisma.product.create({
+    data: {
+      category_id,
+      name,
+      price,
+      description,
+      unit_in_stock,
+    },
+  });
+  res.json(product);
+};
+
+exports.update = async (req, res) => {
+  const { id } = req.params;
+  const { category_id, name, price, description, unit_in_stock } = req.body;
+//   UPDATE Product SET name=?, price=? WHERE id = [id];
+
+  const product = await prisma.product.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      category_id,
+      name,
+      price,
+      description,
+      unit_in_stock,
+    },
+  });
+  res.json(product);
+};
+
+exports.delete = async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json(product);
+};
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+//  get port number from environment settings
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const productRoute = require('./Routes/product.route');
+const customerRoute = require('./Routes/customer.route');
+
+// cross origin resource sharing
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// path url
+// app.[method]();
+
+//  Match Get localhost:4000
+app.get("/", (req,res) => {
+    res.send("สวัสดีเจเกดุ");
+});
+
+// ใช้ productRoute เมื่อ requst ขึ้นต้นด้วย /products
+app.use("/products", productRoute);
+app.use("/customers", customerRoute);
+
+
+app.listen(port, () => {
+    console.log("Server start at port " + port);
+});
